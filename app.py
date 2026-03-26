6a7ef098ddc554ae357607577d96da597380a067
Ritu-Patel13 <rpatel60_be23@thapar.edu>
2026-03-26 14:59:37 +0530

    Added screenshots folder

--- app.py
+++ app.py
@@ -95,8 +95,4 @@ def send_email():
 # RUN APP
 # =========================
 if __name__ == "__main__":
-    if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(debug=True)
